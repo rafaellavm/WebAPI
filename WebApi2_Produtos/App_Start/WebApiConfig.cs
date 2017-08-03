5e7f7bf2dcee88e914f89cd0757b8c1e39aad7a6
Rafaela Veloso Marraschi <rafaela.m.allen@ibge.gov.br>
2017-08-03 14:10:50 -0300

    Subindo o projeto. ASP .NET Web API - Criando e Consumindo uma Web API (C#)

---
+++ WebApi2_Produtos/App_Start/WebApiConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace WebApi2_Produtos
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            //forçar o envio da resposta no formato JSON.
+            var formatters = GlobalConfiguration.Configuration.Formatters;
+            formatters.Remove(formatters.XmlFormatter);
+        }
+    }
+}
---
+++ WebApi2_Produtos/Models/IProdutoRepositorio.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi2_Produtos.Models
+{
+    public interface IProdutoRepositorio
+    {
+        IEnumerable<Produto> GetAll();
+        Produto Get(int id);
+        Produto Add(Produto item);
+        void Remove(int id);
+        bool Update(Produto item);
+    }
+}
